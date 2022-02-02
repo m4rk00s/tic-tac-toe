b6b97eb88dd29000cfc78cc62c5c605d72caa53c
m4rk00s <marktony.x3.24@gmail.com>
2022-02-02 21:24:12 +0700

    update logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import IconX from "./components/Icons/LetterX";
-import IconXOutline from "./components/Icons/OutlinedLetterX";
-import IconO from "./components/Icons/LetterO";
-import IconOOutline from "./components/Icons/OutlinedLetterO";
+import IconX from "./components/icons/LetterX";
+import IconO from "./components/icons/LetterO";
 
 function App(): JSX.Element {
   const navigate = useNavigate();
+  const [playerMark, setPlayerMark] = useState("X");
 
   return (
     <div className="h-full bg-dark-navy flex flex-col items-center justify-center gap-8 px-6">
@@ -20,15 +19,37 @@ function App(): JSX.Element {
           Pick Player 1&apos;s Mark
         </div>
         <div className="flex mt-6 w-full bg-dark-navy rounded-lg p-2">
-          <button type="button" title="X" className="h-14 rounded-xl flex-1">
-            <IconX className="h-8 w-8 text-silver mx-auto" />
+          <button
+            type="button"
+            title="X"
+            className={
+              "h-14 rounded-xl flex-1 " +
+              (playerMark === "X" ? "bg-silver " : "")
+            }
+            onClick={(e) => setPlayerMark("X")}
+          >
+            <IconX
+              className={
+                "h-8 w-8 mx-auto " +
+                (playerMark === "X" ? "text-dark-navy " : "text-silver ")
+              }
+            />
           </button>
           <button
             type="button"
             title="O"
-            className="h-14 rounded-xl flex-1 bg-silver"
+            className={
+              "h-14 rounded-xl flex-1 " +
+              (playerMark === "O" ? "bg-silver " : "")
+            }
+            onClick={(e) => setPlayerMark("O")}
           >
-            <IconO className="h-8 w-8 text-dark-navy mx-auto" />
+            <IconO
+              className={
+                "h-8 w-8 mx-auto " +
+                (playerMark === "O" ? "text-dark-navy " : "text-silver ")
+              }
+            />
           </button>
         </div>
         <div className="uppercase tracking-widest mt-4 text-sm opacity-50">
@@ -39,19 +60,10 @@ function App(): JSX.Element {
       <div className="flex flex-col gap-4 w-full">
         <button
           type="button"
-          className="relative uppercase h-14 bg-light-yellow rounded-2xl shadow-button shadow-[#CC8B13]"
+          className="uppercase h-14 rounded-2xl bg-light-blue shadow-button shadow-[#118C87]"
           onClick={() => navigate("/game")}
         >
-          <span className="font-bold text-dark-navy tracking-widest absolute origin-center flex items-center justify-center mb-2 inset-0">
-            New Game (vs CPU)
-          </span>
-        </button>
-        <button
-          type="button"
-          className="relative uppercase h-14 rounded-2xl bg-light-blue shadow-button shadow-[#118C87]"
-          onClick={() => navigate("/game")}
-        >
-          <span className="font-bold text-dark-navy tracking-widest absolute origin-center flex items-center justify-center mb-2 inset-0">
+          <span className="font-bold text-dark-navy tracking-widest flex items-center justify-center pb-2">
             New Game (vs Player)
           </span>
         </button>
